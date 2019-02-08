e85717e2ec4e8c7cd4debfba99bbe5ce2da60668
ConsUrbinaM <constanzaurbina.m@gmail.com>
2019-02-08 15:23:25 -0300

    primer uso index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-/* Acá va tu código */
+document.getElementById('container1').style.display="block";
+document.getElementById('container2').style.display="none";
+document.getElementById('container3').style.display="none";
+document.getElementById('container4').style.display="none";
+
+
+/*const container=document.getElementById("container1");*/
+
+const firstButton= document.getElementById('btn_enter');
+firstButton.addEventListener('click',() =>{
+  let name= document.getElementById("write_name").value;
+  //document.getElementById("write_name").value="";//
+
+let userText=  document.getElementById('user_name');
+user_name.innerHTML=`<h2>Bienvenida ${name}¿En qué situación te encuentras?</h2>`;
+
+  document.getElementById('container1').style.display="none";
+  document.getElementById('container2').style.display="block";
+})
+
+const secondButton= document.getElementById('btn_select');
+secondButton.addEventListener('click',() => {
+
+document.getElementById('container2').style.display="none";
+document.getElementById('container3').style.display="block";
+
+
+})
+
+const thirdButton= document.getElementById('btn_go');
+thirdButton.addEventListener('click',() =>{
+
+document.getElementById('container3').style.display="none";
+document.getElementById('container4').style.display="block";
+
+})
