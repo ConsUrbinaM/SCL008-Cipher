298e6129e0508634a908b9dbe762fc8706c5048e
ConsUrbinaM <constanzaurbina.m@gmail.com>
2019-02-12 13:55:20 -0300

    prueba test

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -1,5 +1,9 @@
-describe('cipher', () => {
 
+
+
+// a quien vamos a testear, es un arrow function 
+describe('cipher', () => {
+//siempre va dentro del describe, es lo que quiero comprobar, siempre va con "debería" o "should be"
   it('debería ser un objeto', () => {
     assert.equal(typeof cipher, 'object');
   });
@@ -10,8 +14,11 @@ describe('cipher', () => {
       assert.equal(typeof cipher.encode, 'function');
     });
 
-    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33');
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset 33', () => {
+//assert.equal busca la igualdad (dentro pongo la función(dentro pongo los parámetros,parámetro), coma, aquí va el resultado)
+    assert.equal(cipher.encode(33, "ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "HIJKLMNOPQRSTUVWXYZABCDEFG");
   });
+});
 
   describe('cipher.decode', () => {
 
@@ -19,7 +26,9 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offset 33', ()=>{
+    assert.equal(cipher.decode(33,"HIJKLMNOPQRSTUVWXYZABCDEFG"),"ABCDEFGHIJKLMNOPQRSTUVWXYZ" );
   });
 
+});
 });
